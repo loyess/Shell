ff6a276711f663909e4140e86bcaa221e1f5016c
loyess <luoyshappy@gmail.com>
2019-10-18 21:32:23 +0800

    Unclear screen during an entire installation process

--- utils/view_config.sh
+++ utils/view_config.sh
@@ -1,6 +1,9 @@
 show_config(){
+    local mark=$1
     if [ -e $HUMAN_CONFIG ]; then
-        clear -x
+        if [[ ${mark} == "standalone" ]]; then
+            clear -x
+        fi
         cat $HUMAN_CONFIG
     else
         echo "The visual configuration was not found."

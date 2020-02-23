408e4cbab2ceaea20f206963608fc8bffeae97d7
loyess <luoyshappy@gmail.com>
2020-02-23 15:30:06 +0800

    Automatically get RedirAddr based on ServerName

--- prepare/cloak_prepare.sh
+++ prepare/cloak_prepare.sh
@@ -1,10 +1,50 @@
-get_input_rediraddr(){
+auto_get_ip_of_domain(){
+    local domain=$1
+    
+    domain_ip=`ping ${domain} -c 1 2>nul | sed '1{s/[^(]*(//;s/).*//;q}'`
+    rm -fr ./nul
+    if [[ ! -z "${domain_ip}" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+get_input_domain(){
     while true
     do
         echo
-        echo -e "请为Cloak输入重定向ip [留空以将其设置为bing.com的204.79.197.200]"
-        read -e -p "(默认: 204.79.197.200):" ckwebaddr
-        [ -z "$ckwebaddr" ] && ckwebaddr="204.79.197.200"
+        echo -e "请为Cloak输入重定向域名"
+        read -e -p "(默认: www.bing.com):" domain
+        [ -z "$domain" ] && domain="www.bing.com"
+        if [ -z "$(echo $domain | grep -E ${DOMAIN_RE})" ]; then
+            echo
+            echo -e "${Error} 请输入正确合法的域名."
+            echo
+            continue
+        fi
+        
+        if ! auto_get_ip_of_domain ${domain}; then
+            echo
+            echo -e "${Error} 请输入正确合法的域名."
+            echo
+            continue
+        fi
+        
+        echo
+        echo -e "${Red}  ServerName = ${domain}${suffix}"
+        echo 
+        break
+    done
+}
+
+get_input_rediraddr_of_domain(){
+    while true
+    do
+        echo
+        echo -e "请为Cloak输入与重定向域名对应的IP（默认为自动获取）"
+        read -e -p "(默认: ${domain_ip}):" ckwebaddr
+        [ -z "$ckwebaddr" ] && ckwebaddr=${domain_ip}
         if ! is_ipv4_or_ipv6 ${ckwebaddr}; then
             echo
             echo -e "${Error} 请输入正确合法的IP地址."
@@ -16,27 +56,6 @@ get_input_rediraddr(){
         echo -e "${Red}  RedirAddr = ${ckwebaddr}${suffix}"
         echo 
         check_ss_port ${shadowsocksport}
-        break
-    done
-}
-
-get_input_domain_of_rediraddr(){
-    while true
-    do
-        echo
-        echo -e "请为Cloak输入重定向ip相对应的域名"
-        read -e -p "(默认: www.bing.com):" domain
-        [ -z "$domain" ] && domain="www.bing.com"
-        if [ -z "$(echo $domain | grep -E ${DOMAIN_RE})" ]; then
-            echo
-            echo -e "${Error} 请输入正确合法的域名."
-            echo
-            continue
-        fi
-        
-        echo
-        echo -e "${Red}  ServerName = ${domain}${suffix}"
-        echo 
         break
     done
 }
@@ -86,7 +105,7 @@ install_prepare_libev_cloak(){
     if check_port_occupy "443"; then
         echo -e "${Error} 检测到443端口被占用，请排查后重新运行." && exit 1
     fi
-    get_input_rediraddr
-    get_input_domain_of_rediraddr
+    get_input_domain
+    get_input_rediraddr_of_domain
     get_cloak_encryption_method
 }

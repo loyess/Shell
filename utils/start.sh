31c6352b2dcff76f9f8d4f0b2fa1f0e5db691884
loyess <luoyshappy@gmail.com>
2020-04-11 15:57:49 +0800

    Optimization code

--- utils/start.sh
+++ utils/start.sh
@@ -14,6 +14,45 @@ check_running() {
     fi
 }
 
+sip003_plugin_start(){
+    local NAME=$1
+    local PLUGIN_PID=$2
+    local PID_DIR=/var/run
+    local PID_FILE=$PID_DIR/$NAME.pid
+
+    # PID exists. PidFile does not exist. delete PidFile
+    if [ -z ${PLUGIN_PID} ]; then
+        if [ -e ${PID_FILE} ]; then
+            rm -f ${PID_FILE}
+        fi
+    fi
+
+    # Create PidDir
+    if [ ! -d ${PID_DIR} ]; then
+        mkdir -p ${PID_DIR}
+        if [ $? -ne 0 ]; then
+            echo "Creating PID directory $PID_DIR failed"
+            exit 1
+        fi
+    fi
+
+    # Check already running
+    if check_running ${PID_FILE}; then
+        echo "$NAME (pid $PID) is already running."
+        return 0
+    fi
+
+    # Save PID into PidFile
+    echo ${PLUGIN_PID} > ${PID_FILE}
+
+    # Check starting
+    if check_running ${PID_FILE}; then
+        echo "Starting $NAME success"
+    else
+        echo "Starting $NAME failed"
+    fi
+}
+
 shadowsocks_start(){
     if [[ "$(command -v ss-server)" ]]; then
         ${SHADOWSOCKS_LIBEV_INIT} start
@@ -31,37 +70,12 @@ shadowsocks_start(){
 v2ray_plugin_start(){
     if [ "$(command -v v2ray-plugin)" ]; then
         local NAME="v2ray-plugin"
-        local PID_DIR=/var/run
-        local PID_FILE=$PID_DIR/$NAME.pid
-        local V2_PID=`ps -ef |grep -Ev 'grep|-plugin-opts' | grep v2ray-plugin |awk '{print $2}'`
-        
-        if [ -z ${V2_PID} ]; then
-            if [ -e ${PID_FILE} ]; then
-                rm -f ${PID_FILE}
-            fi
-        fi
+        local PID=`ps -ef |grep -Ev 'grep|-plugin-opts' | grep v2ray-plugin |awk '{print $2}'`
 
-        if [ ! -d ${PID_DIR} ]; then
-            mkdir -p ${PID_DIR}
-            if [ $? -ne 0 ]; then
-                echo "Creating PID directory $PID_DIR failed"
-                exit 1
-            fi
-        fi
-        
-        if check_running ${PID_FILE}; then
-            echo "$NAME (pid $PID) is already running."
-            return 0
-        fi
-        
-        echo ${V2_PID} > ${PID_FILE}
-        
-        if check_running ${PID_FILE}; then
-            echo "Starting $NAME success"
-        else
-            echo "Starting $NAME failed"
-        fi
+        sip003_plugin_start "${NAME}" "${PID}"
     fi
+        
+        
 }
 
 kcptun_start(){
@@ -73,72 +87,18 @@ kcptun_start(){
 simple_obfs_start(){
     if [ "$(command -v obfs-server)" ]; then
         local NAME="simple-obfs"
-        local PID_DIR=/var/run
-        local PID_FILE=$PID_DIR/$NAME.pid
-        local OBFS_PID=`ps -ef |grep -Ev 'grep|-plugin-opts' | grep obfs-server |awk '{print $2}'`
-        
-        if [ -z ${OBFS_PID} ]; then
-            if [ -e ${PID_FILE} ]; then
-                rm -f ${PID_FILE}
-            fi
-        fi
+        local PID=`ps -ef |grep -Ev 'grep|-plugin-opts' | grep obfs-server |awk '{print $2}'`
 
-        if [ ! -d ${PID_DIR} ]; then
-            mkdir -p ${PID_DIR}
-            if [ $? -ne 0 ]; then
-                echo "Creating PID directory $PID_DIR failed"
-                exit 1
-            fi
-        fi
-        
-        if check_running ${PID_FILE}; then
-            echo "$NAME (pid $PID) is already running."
-            return 0
-        fi
-        
-        echo ${OBFS_PID} > ${PID_FILE}
-        
-        if check_running ${PID_FILE}; then
-            echo "Starting $NAME success"
-        else
-            echo "Starting $NAME failed"
-        fi    
+        sip003_plugin_start "${NAME}" "${PID}"
     fi
 }
 
 goquiet_start(){
     if [ "$(command -v gq-server)" ]; then
         local NAME="goquiet"
-        local PID_DIR=/var/run
-        local PID_FILE=$PID_DIR/$NAME.pid
-        local GQ_PID=`ps -ef |grep -Ev 'grep|-plugin-opts' | grep gq-server |awk '{print $2}'`
-        
-        if [ -z ${GQ_PID} ]; then
-            if [ -e ${PID_FILE} ]; then
-                rm -f ${PID_FILE}
-            fi
-        fi
+        local PID=`ps -ef |grep -Ev 'grep|-plugin-opts' | grep gq-server |awk '{print $2}'`
 
-        if [ ! -d ${PID_DIR} ]; then
-            mkdir -p ${PID_DIR}
-            if [ $? -ne 0 ]; then
-                echo "Creating PID directory $PID_DIR failed"
-                exit 1
-            fi
-        fi
-        
-        if check_running ${PID_FILE}; then
-            echo "$NAME (pid $PID) is already running."
-            return 0
-        fi
-        
-        echo ${GQ_PID} > ${PID_FILE}
-        
-        if check_running ${PID_FILE}; then
-            echo "Starting $NAME success"
-        else
-            echo "Starting $NAME failed"
-        fi    
+        sip003_plugin_start "${NAME}" "${PID}"
     fi
 }
 
@@ -151,36 +111,9 @@ cloak_start(){
 mtt_start(){
     if [ "$(command -v mtt-server)" ]; then
         local NAME="mos-tls-tunnel"
-        local PID_DIR=/var/run
-        local PID_FILE=$PID_DIR/$NAME.pid
-        local MTT_PID=`ps -ef |grep -Ev 'grep|-plugin-opts' | grep mtt-server |awk '{print $2}'`
-        
-        if [ -z ${MTT_PID} ]; then
-            if [ -e ${PID_FILE} ]; then
-                rm -f ${PID_FILE}
-            fi
-        fi
+        local PID=`ps -ef |grep -Ev 'grep|-plugin-opts' | grep mtt-server |awk '{print $2}'`
 
-        if [ ! -d ${PID_DIR} ]; then
-            mkdir -p ${PID_DIR}
-            if [ $? -ne 0 ]; then
-                echo "Creating PID directory $PID_DIR failed"
-                exit 1
-            fi
-        fi
-        
-        if check_running ${PID_FILE}; then
-            echo "$NAME (pid $PID) is already running."
-            return 0
-        fi
-        
-        echo ${MTT_PID} > ${PID_FILE}
-        
-        if check_running ${PID_FILE}; then
-            echo "Starting $NAME success"
-        else
-            echo "Starting $NAME failed"
-        fi    
+        sip003_plugin_start "${NAME}" "${PID}"
     fi
 }
 
@@ -193,36 +126,9 @@ rabbit_tcp_start(){
 simple_tls_start(){
     if [ "$(command -v simple-tls)" ]; then
         local NAME="simple-tls"
-        local PID_DIR=/var/run
-        local PID_FILE=$PID_DIR/$NAME.pid
-        local SIMPLE_TLS_PID=`ps -ef |grep -Ev 'grep|-plugin-opts' | grep simple-tls |awk '{print $2}'`
+        local PID=`ps -ef |grep -Ev 'grep|-plugin-opts' | grep simple-tls |awk '{print $2}'`
 
-        if [ -z ${SIMPLE_TLS_PID} ]; then
-            if [ -e ${PID_FILE} ]; then
-                rm -f ${PID_FILE}
-            fi
-        fi
-
-        if [ ! -d ${PID_DIR} ]; then
-            mkdir -p ${PID_DIR}
-            if [ $? -ne 0 ]; then
-                echo "Creating PID directory $PID_DIR failed"
-                exit 1
-            fi
-        fi
-
-        if check_running ${PID_FILE}; then
-            echo "$NAME (pid $PID) is already running."
-            return 0
-        fi
-
-        echo ${SIMPLE_TLS_PID} > ${PID_FILE}
-
-        if check_running ${PID_FILE}; then
-            echo "Starting $NAME success"
-        else
-            echo "Starting $NAME failed"
-        fi
+        sip003_plugin_start "${NAME}" "${PID}"
     fi
 }
 

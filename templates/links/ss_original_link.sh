4dbc1c313ad44a3d66b561280e12f318d360ff6f
loyess <luoyshappy@gmail.com>
2021-04-06 00:22:24 +0800

    Fix #70

--- templates/links/ss_original_link.sh
+++ templates/links/ss_original_link.sh
@@ -1,5 +1,7 @@
 # ss
 ss_link(){
-    local tmp=$(get_str_base64_encode "${shadowsockscipher}:${shadowsockspwd}@$(get_ip):${shadowsocksport}")
-    ss_link="ss://${tmp}"
+    local link_head="ss://"
+    local cipher_pwd=$(get_str_base64_encode "${shadowsockscipher}:${shadowsockspwd}")
+    local ip_port="@$(get_ip):${shadowsocksport}"
+    ss_link="${link_head}${cipher_pwd}${ip_port}"
 }

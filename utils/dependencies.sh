325b4d8e36e03a9931b808d0f2bdbfd9f61946b4
loyess <luoyshappy@gmail.com>
2021-08-12 15:17:46 +0800

    Fix #75

--- utils/dependencies.sh
+++ utils/dependencies.sh
@@ -80,7 +80,11 @@ install_dependencies(){
         fi
         [ ! -f /etc/yum.repos.d/epel.repo ] && echo -e "${Error} 安装EPEL存储库失败，请检查它。" && exit 1
         [ ! "$(command -v yum-config-manager)" ] && yum install -y yum-utils > /dev/null 2>&1
-        [ x"$(yum-config-manager epel | grep -w enabled | awk '{print $3}')" != x"True" ] && yum-config-manager --enable epel > /dev/null 2>&1
+        if centosversion 8; then
+            [ x"$(yum repolist epel | grep -w epel | awk '{print $NF}')" != x"enabled" ] && yum-config-manager --enable epel > /dev/null 2>&1
+        else
+            [ x"$(yum-config-manager epel | grep -w enabled | awk '{print $3}')" != x"True" ] && yum-config-manager --enable epel > /dev/null 2>&1
+        fi
         echo -e "${Info} EPEL存储库检查完成."
 
         for depend in ${depends[@]}; do

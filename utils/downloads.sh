fa09b7d0a2cc2d6975d814de443a487318c172bd
loyess <luoyshappy@gmail.com>
2021-12-18 12:45:58 +0800

    Optimization code

--- utils/downloads.sh
+++ utils/downloads.sh
@@ -24,27 +24,75 @@ download_service_file(){
     fi
 }
 
+get_ss_version(){
+    local ssName=$1
+
+    if [ ${ssName} = "shadowsocks-libev" ]; then
+        echo $(wget --no-check-certificate -qO- https://api.github.com/repos/shadowsocks/shadowsocks-libev/releases | grep -o '"tag_name": ".*"' | head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+    elif [ ${ssName} = "shadowsocks-rust" ]; then
+        echo $(wget --no-check-certificate -qO- https://api.github.com/repos/shadowsocks/shadowsocks-rust/releases | grep -o '"tag_name": ".*"' | grep -v 'alpha' | head -n 1 | sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+    elif [ ${ssName} = "go-shadowsocks2" ]; then
+        echo $(wget --no-check-certificate -qO- https://api.github.com/repos/shadowsocks/go-shadowsocks2/releases | grep -o '"tag_name": ".*"' | head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+    fi
+}
+
+get_plugins_version(){
+    local pluginName=$1
+
+    if [ ${pluginName} = "v2ray-plugin" ]; then
+        echo $(wget --no-check-certificate -qO- https://api.github.com/repos/teddysun/v2ray-plugin/releases | grep -o '"tag_name": ".*"' |head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+    elif [ ${pluginName} = "kcptun" ]; then
+        echo $(wget --no-check-certificate -qO- https://api.github.com/repos/xtaci/kcptun/releases | grep -o '"tag_name": ".*"' |head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+    elif [ ${pluginName} = "simple-obfs" ]; then
+        echo $(wget --no-check-certificate -qO- https://api.github.com/repos/shadowsocks/simple-obfs/releases | grep -o '"tag_name": ".*"' | head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+    elif [ ${pluginName} = "GoQuiet" ]; then
+        echo $(wget --no-check-certificate -qO- https://api.github.com/repos/cbeuw/GoQuiet/releases | grep -o '"tag_name": ".*"' |head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+    elif [ ${pluginName} = "Cloak" ]; then
+        echo $(wget --no-check-certificate -qO- https://api.github.com/repos/cbeuw/Cloak/releases | grep -o '"tag_name": ".*"' |head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+    elif [ ${pluginName} = "mos-tls-tunnel" ]; then
+        echo $(wget --no-check-certificate -qO- https://api.github.com/repos/IrineSistiana/mos-tls-tunnel/releases | grep -o '"tag_name": ".*"' |head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+    elif [ ${pluginName} = "rabbit-tcp" ]; then
+        echo $(wget --no-check-certificate -qO- https://api.github.com/repos/ihciah/rabbit-tcp/releases | grep -o '"tag_name": ".*"' |head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+    elif [ ${pluginName} = "simple-tls" ]; then
+        echo $(wget --no-check-certificate -qO- https://api.github.com/repos/IrineSistiana/simple-tls/releases | grep -o '"tag_name": ".*"' | head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+    elif [ ${pluginName} = "gost-plugin" ]; then
+        echo $(wget --no-check-certificate -qO- https://api.github.com/repos/maskedeken/gost-plugin/releases | grep -o '"tag_name": ".*"' | head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+    elif [ ${pluginName} = "xray-plugin" ]; then
+        echo $(wget --no-check-certificate -qO- https://api.github.com/repos/teddysun/xray-plugin/releases | grep -o '"tag_name": ".*"' | head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+    elif [ ${pluginName} = "qtun" ]; then
+        echo $(wget --no-check-certificate -qO- https://api.github.com/repos/shadowsocks/qtun/releases | grep -o '"tag_name": ".*"' | head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+    fi
+}
+
+judge_latest_version_num_is_none_and_output_error_info(){
+    local appName=$1
+    local latestVersion=$2
+
+    [ -z ${latestVersion} ] && echo -e "${Error} 获取${appName}最新版本号失败，退出运行." && exit 1
+}
+
 download_ss_file(){
     cd ${CUR_DIR}
     if [[ ${SS_VERSION} = "ss-libev" ]]; then
-        # Download Shadowsocks-libev
-        libev_ver=$(wget --no-check-certificate -qO- https://api.github.com/repos/shadowsocks/shadowsocks-libev/releases | grep -o '"tag_name": ".*"' | head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
-        [ -z ${libev_ver} ] && echo -e "${Error} 获取 shadowsocks-libev 最新版本失败." && exit 1
-        
+        local ssName="shadowsocks-libev"
+        libev_ver=$(get_ss_version "${ssName}")
+        judge_latest_version_num_is_none_and_output_error_info "${ssName}" "${libev_ver}"
+
         shadowsocks_libev_file="shadowsocks-libev-${libev_ver}"
         shadowsocks_libev_url="https://github.com/shadowsocks/shadowsocks-libev/releases/download/v${libev_ver}/shadowsocks-libev-${libev_ver}.tar.gz"
         download "${shadowsocks_libev_file}.tar.gz" "${shadowsocks_libev_url}"
         download_service_file ${SHADOWSOCKS_LIBEV_INIT} ${SHADOWSOCKS_LIBEV_INIT_ONLINE} ${SHADOWSOCKS_LIBEV_INIT_LOCAL}
     elif [[ ${SS_VERSION} = "ss-rust" ]]; then
-        # Download Shadowsocks-rust
-        rust_ver=$(wget --no-check-certificate -qO- https://api.github.com/repos/shadowsocks/shadowsocks-rust/releases | grep -o '"tag_name": ".*"' | grep -v 'alpha' | head -n 1 | sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
-        [ -z ${rust_ver} ] && echo -e "${Error} 获取 shadowsocks-rust 最新版本失败." && exit 1
+        local ssName="shadowsocks-rust"
+        rust_ver=$(get_ss_version "${ssName}")
+        judge_latest_version_num_is_none_and_output_error_info "${ssName}" "${rust_ver}"
 
         if [[ ${ARCH} = "amd64" ]]; then
             local MACHINE="x86_64"
         else
             local MACHINE="aarch64"
         fi
+
         shadowsocks_rust_file="shadowsocks-v${rust_ver}.${MACHINE}-unknown-linux-gnu"
         shadowsocks_rust_url="https://github.com/shadowsocks/shadowsocks-rust/releases/download/v${rust_ver}/shadowsocks-v${rust_ver}.${MACHINE}-unknown-linux-gnu.tar.xz"
         download "${shadowsocks_rust_file}.tar.xz" "${shadowsocks_rust_url}"
@@ -54,15 +102,17 @@ download_ss_file(){
             echo "[${Red}Error${suffix}] The architecture is not supported."
             exit 1
         fi
-        # Download Go-shadowsocks2
-        go_ver=$(wget --no-check-certificate -qO- https://api.github.com/repos/shadowsocks/go-shadowsocks2/releases | grep -o '"tag_name": ".*"' | head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
-        [ -z ${go_ver} ] && echo -e "${Error} 获取 shadowsocks-rust 最新版本失败." && exit 1
+
+        local ssName="go-shadowsocks2"
+        go_ver=$(get_ss_version "${ssName}")
+        judge_latest_version_num_is_none_and_output_error_info "${ssName}" "${go_ver}"
 
         # wriet version num
         if [ ! -d "$(dirname ${GO_SHADOWSOCKS2_VERSION_FILE})" ]; then
             mkdir -p $(dirname ${GO_SHADOWSOCKS2_VERSION_FILE})
         fi
         echo ${go_ver} > ${GO_SHADOWSOCKS2_VERSION_FILE}
+
         go_shadowsocks2_file="shadowsocks2-linux"
         go_shadowsocks2_url="https://github.com/shadowsocks/go-shadowsocks2/releases/download/v${go_ver}/shadowsocks2-linux.gz"
         download "${go_shadowsocks2_file}.gz" "${go_shadowsocks2_url}"
@@ -73,126 +123,140 @@ download_ss_file(){
 download_plugins_file(){
     cd ${CUR_DIR}
     if [[ "${plugin_num}" == "1" ]]; then        
-        # Download v2ray-plugin
-        v2ray_plugin_ver=$(wget --no-check-certificate -qO- https://api.github.com/repos/teddysun/v2ray-plugin/releases | grep -o '"tag_name": ".*"' |head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
-        [ -z ${v2ray_plugin_ver} ] && echo -e "${Error} 获取 v2ray-plugin 最新版本失败." && exit 1
+        local pluginName="v2ray-plugin"
+        v2ray_plugin_ver=$(get_plugins_version "${pluginName}")
+        judge_latest_version_num_is_none_and_output_error_info "${pluginName}" "${v2ray_plugin_ver}"
+
         v2ray_plugin_file="v2ray-plugin-linux-${ARCH}-v${v2ray_plugin_ver}"
         v2ray_plugin_url="https://github.com/teddysun/v2ray-plugin/releases/download/v${v2ray_plugin_ver}/v2ray-plugin-linux-${ARCH}-v${v2ray_plugin_ver}.tar.gz"
         download "${v2ray_plugin_file}.tar.gz" "${v2ray_plugin_url}"
-        
     elif [[ "${plugin_num}" == "2" ]]; then        
-        # Download kcptun
-        kcptun_ver=$(wget --no-check-certificate -qO- https://api.github.com/repos/xtaci/kcptun/releases | grep -o '"tag_name": ".*"' |head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
-        [ -z ${kcptun_ver} ] && echo -e "${Error} 获取 kcptun 最新版本失败." && exit 1
+        local pluginName="kcptun"
+        kcptun_ver=$(get_plugins_version "${pluginName}")
+        judge_latest_version_num_is_none_and_output_error_info "${pluginName}" "${kcptun_ver}"
+
         kcptun_file="kcptun-linux-${ARCH}-${kcptun_ver}"
         kcptun_url="https://github.com/xtaci/kcptun/releases/download/v${kcptun_ver}/kcptun-linux-${ARCH}-${kcptun_ver}.tar.gz"
         download "${kcptun_file}.tar.gz" "${kcptun_url}"
         download_service_file ${KCPTUN_INIT} ${KCPTUN_INIT_ONLINE} ${KCPTUN_INIT_LOCAL}
-        
     elif [[ "${plugin_num}" == "4" ]]; then        
-        # Download goquiet
-        goquiet_ver=$(wget --no-check-certificate -qO- https://api.github.com/repos/cbeuw/GoQuiet/releases | grep -o '"tag_name": ".*"' |head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
-        [ -z ${goquiet_ver} ] && echo -e "${Error} 获取 goquiet 最新版本失败." && exit 1
+        local pluginName="GoQuiet"
+        goquiet_ver=$(get_plugins_version "${pluginName}")
+        judge_latest_version_num_is_none_and_output_error_info "${pluginName}" "${goquiet_ver}"
+
         goquiet_file="gq-server-linux-${ARCH}-${goquiet_ver}"
         goquiet_url="https://github.com/cbeuw/GoQuiet/releases/download/v${goquiet_ver}/gq-server-linux-${ARCH}-${goquiet_ver}"
         download "${goquiet_file}" "${goquiet_url}"
-        
     elif [[ "${plugin_num}" == "5" ]]; then
-        # Download cloak server
-        cloak_ver=$(wget --no-check-certificate -qO- https://api.github.com/repos/cbeuw/Cloak/releases | grep -o '"tag_name": ".*"' |head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
-        [ -z ${cloak_ver} ] && echo -e "${Error} 获取 cloak 最新版本失败." && exit 1
+        local pluginName="Cloak"
+        cloak_ver=$(get_plugins_version "${pluginName}")
+        judge_latest_version_num_is_none_and_output_error_info "${pluginName}" "${cloak_ver}"
         # cloak_ver="2.1.1"
+
         cloak_file="ck-server-linux-${ARCH}-v${cloak_ver}"
         cloak_url="https://github.com/cbeuw/Cloak/releases/download/v${cloak_ver}/ck-server-linux-${ARCH}-v${cloak_ver}"
         download "${cloak_file}" "${cloak_url}"
         download_service_file ${CLOAK_INIT} ${CLOAK_INIT_ONLINE} ${CLOAK_INIT_LOCAL}
-    
     elif [[ "${plugin_num}" == "6" ]]; then
         if [[ ${ARCH} != "amd64" ]]; then
             echo "[${Red}Error${suffix}] The architecture is not supported."
             exit 1
         fi
-        # Download mos-tls-tunnel
-        mtt_ver=$(wget --no-check-certificate -qO- https://api.github.com/repos/IrineSistiana/mos-tls-tunnel/releases | grep -o '"tag_name": ".*"' |head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
-        [ -z ${mtt_ver} ] && echo -e "${Error} 获取 mos-tls-tunnel 最新版本失败." && exit 1
+
+        local pluginName="mos-tls-tunnel"
+        mtt_ver=$(get_plugins_version "${pluginName}")
+        judge_latest_version_num_is_none_and_output_error_info "${pluginName}" "${mtt_ver}"
+
         # wriet version num
         if [ ! -d "$(dirname ${SHADOWSOCKS_CONFIG})" ]; then
             mkdir -p $(dirname ${SHADOWSOCKS_CONFIG})
         fi
         echo ${mtt_ver} > ${MTT_VERSION_FILE}
+
         mtt_file="mos-tls-tunnel-linux-amd64"
         mtt_url="https://github.com/IrineSistiana/mos-tls-tunnel/releases/download/v${mtt_ver}/mos-tls-tunnel-linux-amd64.zip"
         download "${mtt_file}.zip" "${mtt_url}"
     elif [[ "${plugin_num}" == "7" ]]; then
-        # Download rabbit-tcp
-        rabbit_ver=$(wget --no-check-certificate -qO- https://api.github.com/repos/ihciah/rabbit-tcp/releases | grep -o '"tag_name": ".*"' |head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
-        [ -z ${rabbit_ver} ] && echo -e "${Error} 获取 rabbit-tcp 最新版本失败." && exit 1
+        local pluginName="rabbit-tcp"
+        rabbit_ver=$(get_plugins_version "${pluginName}")
+        judge_latest_version_num_is_none_and_output_error_info "${pluginName}" "${rabbit_ver}"
+
         # wriet version num
         if [ ! -d "$(dirname ${RABBIT_VERSION_FILE})" ]; then
             mkdir -p $(dirname ${RABBIT_VERSION_FILE})
         fi
         echo ${rabbit_ver} > ${RABBIT_VERSION_FILE}
+
         rabbit_file="rabbit-linux-${ARCH}"
         rabbit_url="https://github.com/ihciah/rabbit-tcp/releases/download/v${rabbit_ver}/rabbit-linux-${ARCH}.gz"
         download "${rabbit_file}.gz" "${rabbit_url}"
         download_service_file ${RABBIT_INIT} ${RABBIT_INIT_ONLINE} ${RABBIT_INIT_LOCAL}
     elif [[ "${plugin_num}" == "8" ]]; then
-        # Download simple-tls
-        # simple_tls_ver=$(wget --no-check-certificate -qO- https://api.github.com/repos/IrineSistiana/simple-tls/releases | grep -o '"tag_name": ".*"' | head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+        local pluginName="simple-tls"
+        # simple_tls_ver=$(get_plugins_version "${pluginName}")
         simple_tls_ver="0.5.2"
-        [ -z ${simple_tls_ver} ] && echo -e "${Error} 获取 simple-tls 最新版本失败." && exit 1
+        judge_latest_version_num_is_none_and_output_error_info "${pluginName}" "${simple_tls_ver}"
         if [[ ${SimpleTlsVer} = "1" ]]; then
             simple_tls_ver="0.3.4"
         elif [[ ${SimpleTlsVer} = "2" ]]; then
             simple_tls_ver="0.4.7"
         fi
+
         # wriet version num
         if [ ! -d "$(dirname ${SIMPLE_TLS_VERSION_FILE})" ]; then
             mkdir -p $(dirname ${SIMPLE_TLS_VERSION_FILE})
         fi
         echo ${simple_tls_ver} > ${SIMPLE_TLS_VERSION_FILE}
+
         simple_tls_file="simple-tls-linux-${ARCH}"
         simple_tls_url="https://github.com/IrineSistiana/simple-tls/releases/download/v${simple_tls_ver}/simple-tls-linux-${ARCH}.zip"
         download "${simple_tls_file}.zip" "${simple_tls_url}"
     elif [[ "${plugin_num}" == "9" ]]; then
-        # Download gost-plugin
-        # gost_plugin_ver=$(wget --no-check-certificate -qO- https://api.github.com/repos/maskedeken/gost-plugin/releases | grep -o '"tag_name": ".*"' | head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+        local pluginName="gost-plugin"
+        # gost_plugin_ver=$(get_plugins_version "${pluginName}")
         gost_plugin_ver="1.6.1"
-        [ -z ${gost_plugin_ver} ] && echo -e "${Error} 获取 gost-plugin 最新版本失败." && exit 1
+        judge_latest_version_num_is_none_and_output_error_info "${pluginName}" "${gost_plugin_ver}"
+
         # wriet version num
         if [ ! -d "$(dirname ${GOST_PLUGIN_VERSION_FILE})" ]; then
             mkdir -p $(dirname ${GOST_PLUGIN_VERSION_FILE})
         fi
         echo ${gost_plugin_ver} > ${GOST_PLUGIN_VERSION_FILE}
+
         gost_plugin_file="gost-plugin_linux_${ARCH}-${gost_plugin_ver}"
         gost_plugin_url="https://github.com/maskedeken/gost-plugin/releases/download/v${gost_plugin_ver}/${gost_plugin_file}.zip"
         download "${gost_plugin_file}.zip" "${gost_plugin_url}"
     elif [[ "${plugin_num}" == "10" ]]; then
-        # Download xray-plugin
-        xray_plugin_ver=$(wget --no-check-certificate -qO- https://api.github.com/repos/teddysun/xray-plugin/releases | grep -o '"tag_name": ".*"' | head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
-        [ -z ${xray_plugin_ver} ] && echo -e "${Error} 获取 xray-plugin 最新版本失败." && exit 1
+        local pluginName="xray-plugin"
+        xray_plugin_ver=$(get_plugins_version "${pluginName}")
+        judge_latest_version_num_is_none_and_output_error_info "${pluginName}" "${xray_plugin_ver}"
+
         # wriet version num
         if [ ! -d "$(dirname ${XRAY_PLUGIN_VERSION_FILE})" ]; then
             mkdir -p $(dirname ${XRAY_PLUGIN_VERSION_FILE})
         fi
         echo ${xray_plugin_ver} > ${XRAY_PLUGIN_VERSION_FILE}
+
         xray_plugin_file="xray-plugin-linux-${ARCH}-v${xray_plugin_ver}"
         xray_plugin_url="https://github.com/teddysun/xray-plugin/releases/download/v${xray_plugin_ver}/xray-plugin-linux-${ARCH}-v${xray_plugin_ver}.tar.gz"
         download "${xray_plugin_file}.tar.gz" "${xray_plugin_url}"
     elif [[ "${plugin_num}" == "11" ]]; then
-        # Download qtun
-        qtun_ver=$(wget --no-check-certificate -qO- https://api.github.com/repos/shadowsocks/qtun/releases | grep -o '"tag_name": ".*"' | head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
-        [ -z ${qtun_ver} ] && echo -e "${Error} 获取 qtun 最新版本失败." && exit 1
+        local pluginName="qtun"
+        qtun_ver=$(get_plugins_version "${pluginName}")
+        judge_latest_version_num_is_none_and_output_error_info "${pluginName}" "${qtun_ver}"
+
         # wriet version num
         if [ ! -d "$(dirname ${QTUN_VERSION_FILE})" ]; then
             mkdir -p $(dirname ${QTUN_VERSION_FILE})
         fi
         echo ${qtun_ver} > ${QTUN_VERSION_FILE}
+
         if [[ ${ARCH} = "amd64" ]]; then
             local MACHINE="x86_64"
         else
             local MACHINE="aarch64"
         fi
+
         qtun_file="qtun-v${qtun_ver}.${MACHINE}-unknown-linux-musl"
         qtun_url="https://github.com/shadowsocks/qtun/releases/download/v${qtun_ver}/qtun-v${qtun_ver}.${MACHINE}-unknown-linux-musl.tar.xz"
         download "${qtun_file}.tar.xz" "${qtun_url}"
--- utils/update.sh
+++ utils/update.sh
@@ -14,14 +14,7 @@ judge_current_version_num_is_none_and_output_error_info(){
     local appName=$1
     local currentVersion=$2
     
-    [ -z ${currentVersion} ] && echo -e "${Error} 获取${appName}当前版本号失败，退出运行." && exit 0
-}
-
-judge_latest_version_num_is_none_and_output_error_info(){
-    local appName=$1
-    local latestVersion=$2
-    
-    [ -z ${latestVersion} ] && echo -e "${Error} 获取${appName}最新版本号失败，退出运行." && exit 0
+    [ -z ${currentVersion} ] && echo -e "${Error} 获取${appName}当前版本号失败，退出运行." && exit 1
 }
 
 judge_not_update_and_output_point_info(){
@@ -90,89 +83,89 @@ read_version_num(){
     echo ${versionNum}
 }
 
-get_ss_version(){
+ss_update_preparation(){
     ssUpdatePackageName="tools"
     if [[ -e ${SHADOWSOCKS_LIBEV_BIN_PATH} ]]; then
         ssNameUpdate="shadowsocks-libev"
         ssUpdateDownloadMark="ss-libev"
         ssUpdateShFileName="shadowsocks_install.sh"
         ssUpdateCalledFuncName="install_shadowsocks_libev"
-        ssLatestVersion=$(wget --no-check-certificate -qO- https://api.github.com/repos/shadowsocks/shadowsocks-libev/releases | grep -o '"tag_name": ".*"' | head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+        ssLatestVersion=$(get_ss_version "${ssNameUpdate}")
         ssCurrentVersion=$(ss-server -v | grep shadowsocks-libev | cut -d\  -f2)
     elif [[ -e ${SHADOWSOCKS_RUST_BIN_PATH} ]]; then
         ssNameUpdate="shadowsocks-rust"
         ssUpdateDownloadMark="ss-rust"
         ssUpdateShFileName="shadowsocks_install.sh"
         ssUpdateCalledFuncName="install_shadowsocks_rust"
-        ssLatestVersion=$(wget --no-check-certificate -qO- https://api.github.com/repos/shadowsocks/shadowsocks-rust/releases | grep -o '"tag_name": ".*"' | grep -v 'alpha' | head -n 1 | sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+        ssLatestVersion=$(get_ss_version "${ssNameUpdate}")
         ssCurrentVersion=$(ssserver -V | grep shadowsocks | cut -d\  -f2)
     elif [[ -e ${GO_SHADOWSOCKS2_BIN_PATH} ]]; then
         ssNameUpdate="go-shadowsocks2"
         ssUpdateDownloadMark="go-ss2"
         ssUpdateShFileName="shadowsocks_install.sh"
         ssUpdateCalledFuncName="install_go_shadowsocks2"
-        ssLatestVersion=$(wget --no-check-certificate -qO- https://api.github.com/repos/shadowsocks/go-shadowsocks2/releases | grep -o '"tag_name": ".*"' | head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+        ssLatestVersion=$(get_ss_version "${ssNameUpdate}")
         ssCurrentVersion=$(read_version_num "${GO_SHADOWSOCKS2_VERSION_FILE}")
     fi
 }
 
-get_plugins_version(){
+plugins_update_preparation(){
     pluginUpdatePackageName="plugins"
     if [[ -e ${V2RAY_PLUGIN_BIN_PATH} ]]; then
         pluginNameUpdate="v2ray-plugin"
         pluginUpdateDownloadMark="1"
         pluginUpdateShFileName="v2ray_plugin_install.sh"
         pluginUpdateCalledFuncName="install_v2ray_plugin"
-        pluginLatestVersion=$(wget --no-check-certificate -qO- https://api.github.com/repos/teddysun/v2ray-plugin/releases | grep -o '"tag_name": ".*"' |head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+        pluginLatestVersion=$(get_plugins_version "${pluginNameUpdate}")
         pluginCurrentVersion=$(v2ray-plugin -version | grep v2ray-plugin | cut -d\  -f2 | sed 's/v//g')
     elif [[ -e ${KCPTUN_BIN_PATH} ]]; then
         pluginNameUpdate="kcptun"
         pluginUpdateDownloadMark="2"
         pluginUpdateShFileName="kcptun_install.sh"
         pluginUpdateCalledFuncName="install_kcptun"
-        pluginLatestVersion=$(wget --no-check-certificate -qO- https://api.github.com/repos/xtaci/kcptun/releases | grep -o '"tag_name": ".*"' |head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+        pluginLatestVersion=$(get_plugins_version "${pluginNameUpdate}")
         pluginCurrentVersion=$(kcptun-server -v | grep kcptun | cut -d\  -f3)
     elif [[ -e ${SIMPLE_OBFS_BIN_PATH} ]]; then
         pluginNameUpdate="simple-obfs"
         pluginUpdateDownloadMark="3"
         pluginUpdateShFileName="simple_obfs_install.sh"
         pluginUpdateCalledFuncName="install_simple_obfs"
-        pluginLatestVersion=$(wget --no-check-certificate -qO- https://api.github.com/repos/shadowsocks/simple-obfs/releases | grep -o '"tag_name": ".*"' | head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+        pluginLatestVersion=$(get_plugins_version "${pluginNameUpdate}")
         pluginCurrentVersion=$(obfs-server | grep simple-obfs | cut -d\  -f2)
     elif [[ -e ${GOQUIET_BIN_PATH} ]]; then
         pluginNameUpdate="GoQuiet"
         pluginUpdateDownloadMark="4"
         pluginUpdateShFileName="goquiet_install.sh"
         pluginUpdateCalledFuncName="install_goquiet"
-        pluginLatestVersion=$(wget --no-check-certificate -qO- https://api.github.com/repos/cbeuw/GoQuiet/releases | grep -o '"tag_name": ".*"' |head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+        pluginLatestVersion=$(get_plugins_version "${pluginNameUpdate}")
         pluginCurrentVersion=$(gq-server -v | grep gq-server | cut -d\  -f2)
     elif [[ -e ${CLOAK_SERVER_BIN_PATH} ]]; then
         pluginNameUpdate="Cloak"
         pluginUpdateDownloadMark="5"
         pluginUpdateShFileName="cloak_install.sh"
         pluginUpdateCalledFuncName="install_cloak"
-        pluginLatestVersion=$(wget --no-check-certificate -qO- https://api.github.com/repos/cbeuw/Cloak/releases | grep -o '"tag_name": ".*"' |head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+        pluginLatestVersion=$(get_plugins_version "${pluginNameUpdate}")
         pluginCurrentVersion=$(ck-server -v | grep ck-server | cut -d\  -f2 | sed 's/v//g')
     elif [[ -e ${MTT_BIN_PATH} ]]; then
         pluginNameUpdate="mos-tls-tunnel"
         pluginUpdateDownloadMark="6"
         pluginUpdateShFileName="mos_tls_tunnel_install.sh"
         pluginUpdateCalledFuncName="install_mos_tls_tunnel"
-        pluginLatestVersion=$(wget --no-check-certificate -qO- https://api.github.com/repos/IrineSistiana/mos-tls-tunnel/releases | grep -o '"tag_name": ".*"' |head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+        pluginLatestVersion=$(get_plugins_version "${pluginNameUpdate}")
         pluginCurrentVersion=$(read_version_num "${MTT_VERSION_FILE}")
     elif [[ -e ${RABBIT_BIN_PATH} ]]; then
-        pluginNameUpdate="rabbit-Tcp"
+        pluginNameUpdate="rabbit-tcp"
         pluginUpdateDownloadMark="7"
         pluginUpdateShFileName="rabbit_tcp_install.sh"
         pluginUpdateCalledFuncName="install_rabbit_tcp"
-        pluginLatestVersion=$(wget --no-check-certificate -qO- https://api.github.com/repos/ihciah/rabbit-tcp/releases | grep -o '"tag_name": ".*"' |head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+        pluginLatestVersion=$(get_plugins_version "${pluginNameUpdate}")
         pluginCurrentVersion=$(read_version_num "${RABBIT_VERSION_FILE}")
     elif [[ -e ${SIMPLE_TLS_BIN_PATH} ]]; then
         pluginNameUpdate="simple-tls"
         pluginUpdateDownloadMark="8"
         pluginUpdateShFileName="simple_tls_install.sh"
         pluginUpdateCalledFuncName="install_simple_tls"
-        # pluginLatestVersion=$(wget --no-check-certificate -qO- https://api.github.com/repos/IrineSistiana/simple-tls/releases | grep -o '"tag_name": ".*"' | head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+        # pluginLatestVersion=$(get_plugins_version "${pluginNameUpdate}")
         pluginLatestVersion=0.5.2
         pluginCurrentVersion=$(read_version_num "${SIMPLE_TLS_VERSION_FILE}")
     elif [[ -e ${GOST_PLUGIN_BIN_PATH} ]]; then
@@ -180,7 +173,7 @@ get_plugins_version(){
         pluginUpdateDownloadMark="9"
         pluginUpdateShFileName="gost_plugin_install.sh"
         pluginUpdateCalledFuncName="install_gost_plugin"
-        # pluginLatestVersion=$(wget --no-check-certificate -qO- https://api.github.com/repos/maskedeken/gost-plugin/releases | grep -o '"tag_name": ".*"' | head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+        # pluginLatestVersion=$(get_plugins_version "${pluginNameUpdate}")
         pluginLatestVersion=1.6.1
         pluginCurrentVersion=$(read_version_num "${GOST_PLUGIN_VERSION_FILE}")
     elif [[ -e ${XRAY_PLUGIN_BIN_PATH} ]]; then
@@ -188,14 +181,14 @@ get_plugins_version(){
         pluginUpdateDownloadMark="10"
         pluginUpdateShFileName="xray_plugin_install.sh"
         pluginUpdateCalledFuncName="install_xray_plugin"
-        pluginLatestVersion=$(wget --no-check-certificate -qO- https://api.github.com/repos/teddysun/xray-plugin/releases | grep -o '"tag_name": ".*"' | head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+        pluginLatestVersion=$(get_plugins_version "${pluginNameUpdate}")
         pluginCurrentVersion=$(read_version_num "${XRAY_PLUGIN_VERSION_FILE}")
     elif [[ -e ${QTUN_BIN_PATH} ]]; then
         pluginNameUpdate="qtun"
         pluginUpdateDownloadMark="11"
         pluginUpdateShFileName="qtun_install.sh"
         pluginUpdateCalledFuncName="install_qtun"
-        pluginLatestVersion=$(wget --no-check-certificate -qO- https://api.github.com/repos/shadowsocks/qtun/releases | grep -o '"tag_name": ".*"' | head -n 1| sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
+        pluginLatestVersion=$(get_plugins_version "${pluginNameUpdate}")
         pluginCurrentVersion=$(read_version_num "${QTUN_VERSION_FILE}")
     fi
 }
@@ -254,7 +247,7 @@ update_caddy_v2(){
 }
 
 ss_update_logic(){
-    get_ss_version
+    ss_update_preparation
     judge_current_version_num_is_none_and_output_error_info "${ssNameUpdate}" "${ssCurrentVersion}"
     judge_latest_version_num_is_none_and_output_error_info "${ssNameUpdate}" "${ssLatestVersion}"
     judge_not_update_and_output_point_info "${ssNameUpdate}" "${ssCurrentVersion}" "${ssLatestVersion}"
@@ -265,7 +258,7 @@ ss_update_logic(){
 }
 
 plugins_update_logic(){
-    get_plugins_version
+    plugins_update_preparation
     judge_current_version_num_is_none_and_output_error_info "${pluginNameUpdate}" "${pluginCurrentVersion}"
     judge_latest_version_num_is_none_and_output_error_info "${pluginNameUpdate}" "${pluginLatestVersion}"
     judge_not_update_when_simple_tls_is_specified_version "${pluginNameUpdate}" "${pluginCurrentVersion}"
